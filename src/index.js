e2c1bff948185731705dbce2645bd76640d451f9
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-04-26 14:33:34 -0300

    Inserção de lembrete criado

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ const express = require ('express');
 const app = express();
 app.use(express.json());
 const lembretes = {};
+const axios = require("axios");
 contador = 0;
 app.get ('/lembretes', (req, res) => {
     res.send(lembretes);
 });
-app.put ('/lembretes', (req, res) => {
+app.put ('/lembretes', async (req, res) => {
     contador++;
     const { texto } = req.body;
     lembretes[contador] = {
     contador, texto
     }
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        }
+    })
     res.status(201).send(lembretes[contador])
 });
 
