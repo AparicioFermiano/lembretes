fa847f02ffb3aae1d53600b2e35457a8d6a224d4
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-05-03 18:45:01 -0300

    Atualização 1.2

--- src/index.js
+++ src/index.js
@@ -22,7 +22,10 @@ app.put ('/lembretes', async (req, res) => {
     })
     res.status(201).send(lembretes[contador])
 });
-
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({msg: "ok"});
+});
 app.listen(4000, () => {
 console.log('Lembretes. Porta 4000');
 });
